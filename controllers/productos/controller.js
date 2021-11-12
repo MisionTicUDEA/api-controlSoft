54ba2ef56e73864f6c615e1f6da52d628e440cfa
CarlosMezai <90356626+CarlosMezai@users.noreply.github.com>
2021-11-11 23:59:07 -0500

    Version 1.0 Backend proyecto Ciclo 3 Ufea MinTic

---
+++ controllers/productos/controller.js
@@ -0,0 +1,46 @@
+import { ObjectId } from 'mongodb';
+import { getDB } from '../../db/db.js';
+
+const queryAllProducts = async (callback) => {
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('producto').find({}).limit(50).toArray(callback);
+};
+
+const crearProducto = async (datosProducto, callback) => {
+  if (
+    Object.keys(datosProducto).includes('name') &&
+    Object.keys(datosProducto).includes('kind') &&
+    Object.keys(datosProducto).includes('size')
+  ) {
+    const baseDeDatos = getDB();
+    // implementar código para crear vehículo en la BD
+
+    await baseDeDatos.collection('producto').insertOne(datosProducto, callback);
+  } else {
+    return 'error';
+  }
+};
+
+const consultarProducto = async (id, callback) => {
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('producto').findOne({ _id: new ObjectId(id) }, callback);
+};
+
+const editarProducto = async (id, edicion, callback) => {
+  const filtroProducto = { _id: new ObjectId(id) };
+  const operacion = {
+    $set: edicion,
+  };
+  const baseDeDatos = getDB();
+  await baseDeDatos
+    .collection('producto')
+    .findOneAndUpdate(filtroProducto, operacion, { upsert: true, returnOriginal: true }, callback);
+};
+
+const eliminarProducto = async (id, callback) => {
+  const filtroProducto = { _id: new ObjectId(id) };
+  const baseDeDatos = getDB();
+  await baseDeDatos.collection('producto').deleteOne(filtroProducto, callback);
+};
+
+export { queryAllProducts, crearProducto, consultarProducto, editarProducto, eliminarProducto };
---
+++ server.js
@@ -0,0 +1,53 @@
+// hacer el import de express tradicional
+// const express = require('express');
+
+// hacer el nuevo import
+import Express from 'express';
+import Cors from 'cors';
+import dotenv from 'dotenv';
+import { conectarBD } from './db/db.js';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+
+import rutasVehiculo from './views/productos/rutas.js';
+import rutasUsuario from './views/usuarios/rutas.js';
+import rutasVenta from './views/ventas/rutas.js';
+import autorizacionEstadoUsuario from './middleware/autorizacionEstadoUsuario.js';
+
+dotenv.config({ path: './.env' });
+
+const port = process.env.PORT || 5000;
+
+const app = Express();
+
+app.use(Express.json());
+app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://misiontic-ventas-ciclo-tres.us.auth0.com/.well-known/jwks.json'
+}),
+audience: 'api-autenticacion-ventas-mintic',
+issuer: 'https://misiontic-ventas-ciclo-tres.us.auth0.com/',
+algorithms: ['RS256']
+});
+
+// 4 y 5: enviarle el token a auth0 para que devuelva si es valido o no
+app.use(jwtCheck);
+
+app.use(autorizacionEstadoUsuario);
+
+app.use(rutasVehiculo);
+app.use(rutasUsuario);
+app.use(rutasVenta);
+
+const main = () => {
+  return app.listen(port, () => {
+    console.log(`escuchando puerto ${port}`);
+  });
+};
+
+conectarBD(main);
